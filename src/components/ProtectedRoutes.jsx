13d6ad89e8a98fa8acd242cd7f52bd7e2335d4b9
joelfranklin22 <joeljeyam22@gmail.com>
2026-09-06 18:21:57 +0530

    Protected Routed for Login,Register and Chat

---
+++ src/components/ProtectedRoutes.jsx
@@ -0,0 +1,9 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+function ProtectedRoutes() {
+  const token = localStorage.getItem("token");
+  if (!token) return <Navigate to={"/login"} />;
+  return <Outlet />;
+}
+
+export default ProtectedRoutes;
