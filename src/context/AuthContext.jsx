5ed3ac205510fe16556c58ace2dea895a73c1089
joelfranklin22 <joeljeyam22@gmail.com>
2026-09-08 22:48:30 +0530

    Context for AccessToken to store

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+const AuthContext = createContext();
+export function AuthProvider({ children }) {
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+}
+
+export default AuthContext;
