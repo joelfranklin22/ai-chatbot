7cb7048d0092abe2c88257e406d349c4002f1b6d
joelfranklin22 <joeljeyam22@gmail.com>
2026-09-08 22:45:02 +0530

    Server updated with CookieParser,loginRoutes,registerRoutes

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
 import db from "./config/database.js";
-import authroutes from "./routes/authroutes.js";
+import registerRoutes from "./routes/authRegister.js";
+import loginRoutes from "./routes/authLogin.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth", authroutes);
+app.use("/api/auth", registerRoutes);
+app.use("/api/auth", loginRoutes);
 
 app.listen(4000, () => {
   console.log("Server started Running");
