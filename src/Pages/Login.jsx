1924517a18a97a3b2e00ac6a77c06c91e34f141d
joelfranklin22 <joeljeyam22@gmail.com>
2026-09-09 23:01:38 +0530

    Validated forms,Data sent to backend

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,25 +1,82 @@
+import { useForm } from "react-hook-form";
 import styles from "../styles/Auth.module.css";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { checkUser } from "../api/authApi";
+import { useState } from "react";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const schema = z.object({
+    email: z.string().email("Invalid Email"),
+    password: z.string().min(6, "Password must be 6 characters"),
+  });
+  const [loginMsg, setLoginMsg] = useState(false);
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  const sendData = async (data) => {
+    try {
+      const res = await checkUser(data.email, data.password);
+
+      if (res.status == 200) {
+        setLoginMsg(true);
+        navigate("/chat");
+      }
+
+      console.log(res.data);
+
+      console.log(data);
+      reset();
+    } catch (error) {
+      console.log(error.response?.data?.msg || "login failed");
+    }
+  };
+
   return (
     <>
       <div className={styles.container}>
         <div className={styles.loginContainer}>
           <h2>Welcome Back!</h2>
-          <form action="">
+          <form onSubmit={handleSubmit(sendData)}>
             <div className={styles.formGroup}>
               <label>Email</label>
-              <input type="email" name="" id="" />
-              <div>invalid email</div>
+              <input
+                type="email"
+                name=""
+                id=""
+                {...register("email")}
+                placeholder="e.g., name@example.com"
+              />
+              {errors.email && (
+                <div className={styles.errorMsg}>{errors.email.message}</div>
+              )}
             </div>
             <div className={styles.formGroup}>
               <label>Password</label>
-              <input type="password" name="" id="" />
-              <div>invalid password</div>
+              <input
+                type="password"
+                name=""
+                id=""
+                {...register("password")}
+                placeholder="Password must be 6 characters"
+              />
+              {errors.password && (
+                <div className={styles.errorMsg}>{errors.password.message}</div>
+              )}
             </div>
-            <button className={styles.loginBtn}>Sign in</button>
+            {loginMsg && (
+              <div className={styles.loginMsg}>Login SuccessFull</div>
+            )}
+            <button type="submit" className={styles.loginBtn}>
+              Sign in
+            </button>
           </form>
           <div className={styles.divider}>
             <span>or</span>
