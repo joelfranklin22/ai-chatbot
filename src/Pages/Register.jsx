d38967283c671c9a255f6e1f571259d25ae4a8ce
joelfranklin22 <joeljeyam22@gmail.com>
2026-09-09 23:02:11 +0530

    Ui updated while register successfull

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -6,9 +6,10 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-
 function Register() {
   const [serverError, setServerError] = useState("");
+  const [loginMsg, setLoginMsg] = useState(false);
+
   // Form Validation
   const schema = z.object({
     name: z.string().min(1, "Name is Required"),
@@ -33,9 +34,12 @@ function Register() {
       const res = await addUser(data.name, data.email, data.password);
       console.log(res.data.token);
       localStorage.setItem("token", res.data.token);
-      navigate("/chat");
+      setLoginMsg(true);
+      setTimeout(() => {
+        navigate("/chat");
+        reset();
+      }, 700);
 
-      reset();
     } catch (error) {
       setServerError(error.response?.data?.msg || "Something went wrong");
     }
@@ -51,7 +55,13 @@ function Register() {
           <form onSubmit={handleSubmit(sendData)}>
             <div className={styles.formGroup}>
               <label>Name</label>
-              <input type="text" name="" id="" {...register("name")} />
+              <input
+                type="text"
+                name=""
+                id=""
+                {...register("name")}
+                placeholder="Enter your name"
+              />
               {errors.name && (
                 <div className={styles.errorMsg}>
                   <strong>{errors.name.message}</strong>
@@ -60,7 +70,13 @@ function Register() {
             </div>
             <div className={styles.formGroup}>
               <label>Email</label>
-              <input type="email" name="" id="" {...register("email")} />
+              <input
+                type="email"
+                name=""
+                id=""
+                {...register("email")}
+                placeholder="e.g., name@example.com"
+              />
               {errors.email && (
                 <div className={styles.errorMsg}>
                   <strong>{errors.email.message}</strong>
@@ -69,7 +85,14 @@ function Register() {
             </div>
             <div className={styles.formGroup}>
               <label>Password</label>
-              <input type="password" name="" id="" {...register("password")} />
+
+              <input
+                type="password"
+                name=""
+                id=""
+                {...register("password")}
+                placeholder="Password must be 6 characters"
+              />
               {errors.password && (
                 <div className={styles.errorMsg}>
                   <strong>{errors.password.message}</strong>
@@ -78,6 +101,9 @@ function Register() {
             </div>
             {serverError && (
               <div className={styles.errorMsg}>{serverError}</div>
+            )}
+            {loginMsg && (
+              <div className={styles.loginMsg}>Login SuccessFull</div>
             )}
             <button type="submit" className={styles.loginBtn}>
               Create account
