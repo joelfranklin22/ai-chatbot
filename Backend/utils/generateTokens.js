53bb60a515374925026f59e0c8f0a96ff2f03918
joelfranklin22 <joeljeyam22@gmail.com>
2026-09-09 22:59:03 +0530

    Access and refresh tokens generated

--- Backend/utils/generateTokens.js
+++ Backend/utils/generateTokens.js
@@ -3,10 +3,10 @@ import jwt from "jsonwebtoken";
 
 
 export const generateAccessTokens = (userId) => {
-  return jwt.sign({ userId }, "my-sceret-key", { expiresIn: "1hr" });
+  return jwt.sign({ userId }, "Happy", { expiresIn: "1hr" });
 };
 
 
 export const generateRefreshTokens = (userId) => {
-  return jwt.sign({ userId }, "my-sceret-key-refresh", { expiresIn: "7d" });
+  return jwt.sign({ userId }, "Happy", { expiresIn: "7d" });
 };
