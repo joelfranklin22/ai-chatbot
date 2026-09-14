4bd7ddb51d4bad04ae298b1d0ffa9da61930ced4
joelfranklin22 <joeljeyam22@gmail.com>
2026-09-14 20:01:44 +0530

    Context for tokens added in Router

--- src/router/PageRouter.jsx
+++ src/router/PageRouter.jsx
@@ -3,20 +3,23 @@ import Register from "../Pages/Register";
 import Login from "../Pages/Login";
 import Chat from "../Pages/Chat";
 import ProtectedRoutes from "../components/ProtectedRoutes";
+import { AuthProvider } from "../context/AuthContext";
 
 function PageRouter() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/chat" element={<Chat />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/chat" element={<Chat />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+     </>
   );
 }
 
