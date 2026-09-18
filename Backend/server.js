f8c414b27629bc7fd3be86918d789c5a9f19fec0
joelfranklin22 <joeljeyam22@gmail.com>
2026-09-18 21:43:19 +0530

    cors and routes updated

--- Backend/server.js
+++ Backend/server.js
@@ -5,15 +5,22 @@ import registerRoutes from "./routes/authRegister.js";
 import loginRoutes from "./routes/authLogin.js";
 import chatRoutes from "./routes/chat.js";
 import cookieParser from "cookie-parser";
+import refreshToken from "./utils/refreshToken.js";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", registerRoutes);
 app.use("/api/auth", loginRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/refreshtokens", refreshToken);
 
 app.listen(4000, () => {
   console.log("Server started Running");
