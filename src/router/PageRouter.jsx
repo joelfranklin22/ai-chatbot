b0848071234bd0b82009e27403e28d14e3483060
joelfranklin22 <joeljeyam22@gmail.com>
2026-09-18 21:46:12 +0530

    Refresh Tokens checked

--- src/router/PageRouter.jsx
+++ src/router/PageRouter.jsx
@@ -1,25 +1,47 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Register from "../Pages/Register";
 import Login from "../Pages/Login";
 import Chat from "../Pages/Chat";
 import ProtectedRoutes from "../components/ProtectedRoutes";
-import { AuthProvider } from "../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function PageRouter() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    async function checkTokens() {
+      try {
+        const res = await axios.post(
+          "http://localhost:4000/api/refreshtokens/generate",
+          {},
+          { withCredentials: true },
+        );
+        if (!res) console.log("no tokens");
+
+        console.log(res);
+        console.log(res.data.tokens);
+      } catch (error) {
+        setTimeout(() => {
+          navigate("/login");
+        }, 3000);
+        console.log("login again to get tokens", error);
+      }
+    }
+    checkTokens();
+  }, []);
   return (
     <>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route element={<ProtectedRoutes />}>
-              <Route path="/chat" element={<Chat />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-     </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/chat" element={<Chat />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
