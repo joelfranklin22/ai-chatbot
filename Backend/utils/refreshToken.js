2ce77d297366422f32ce27e082fcb2f2f7e1829d
joelfranklin22 <joeljeyam22@gmail.com>
2026-09-18 21:46:35 +0530

    Refresh token generation

---
+++ Backend/utils/refreshToken.js
@@ -0,0 +1,23 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post("/generate", (req, res) => {
+
+  const refreshTokens = req.cookies.refreshToken;
+  console.log(refreshTokens);
+  
+  if (!refreshTokens) return res.status(401).json({ msg: "no refresh tokens" });
+
+  jwt.verify(refreshTokens, "refreshToken", (err, decoded) => {
+    if (err) return res.status(401).json({ msg: "tokens expired" });
+
+    const accessToken = jwt.sign({ userId: decoded.userId }, "Happy", {
+      expiresIn: "1h",
+    });
+    res.json({ accessToken });
+  });
+});
+
+export default router;
